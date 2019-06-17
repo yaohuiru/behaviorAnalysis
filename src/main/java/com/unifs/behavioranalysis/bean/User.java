01dbfe2f55761aa4bd69621f196e3ea4d8a819bb
sunshine305 <18275880040@163.com>
2019-06-17 16:24:59 +0800

    AreaInfo错误修正

--- src/main/java/com/unifs/behavioranalysis/bean/User.java
+++ src/main/java/com/unifs/behavioranalysis/bean/User.java
@@ -1,5 +1,8 @@
 package com.unifs.behavioranalysis.bean;
 
+import lombok.Data;
+
+@Data
 public class User {
     private String userId;
 
@@ -15,59 +18,5 @@ public class User {
 
     private String state;
 
-    public String getUserId() {
-        return userId;
-    }
 
-    public void setUserId(String userId) {
-        this.userId = userId == null ? null : userId.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getUserNum() {
-        return userNum;
-    }
-
-    public void setUserNum(String userNum) {
-        this.userNum = userNum == null ? null : userNum.trim();
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail == null ? null : userEmail.trim();
-    }
-
-    public String getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(String departmentId) {
-        this.departmentId = departmentId == null ? null : departmentId.trim();
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state == null ? null : state.trim();
-    }
 }
