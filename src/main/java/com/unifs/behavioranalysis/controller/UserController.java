05ef8851fcb40de320697fdc7c5f7e18f94af3d2
fangxl11 <fangxl11@chinaunicom.cn>
2019-06-18 10:41:39 +0800

    Merge branch 'master' of https://github.com/yaohuiru/behaviorAnalysis
    # Conflicts:
    #	src/main/java/com/unifs/behavioranalysis/config/WebConfig.java
    #	src/main/java/com/unifs/behavioranalysis/dao/UserMapper.java

--- src/main/java/com/unifs/behavioranalysis/controller/UserController.java
+++ src/main/java/com/unifs/behavioranalysis/controller/UserController.java
@@ -1,28 +1,45 @@
 package com.unifs.behavioranalysis.controller;
 
 
+import com.unifs.behavioranalysis.base.Resp;
 import com.unifs.behavioranalysis.bean.User;
+import com.unifs.behavioranalysis.enums.RespCode;
+import com.unifs.behavioranalysis.service.impl.UserServiceImpl;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(value = "UserController", description = "用户操作控制层")
 @RequestMapping(value = "/user")
 public class UserController
 {
-
+    @Autowired
+    private UserServiceImpl userserviceimpl;
     @PostMapping(value = "/add")
-    public void addUser(@RequestBody @ApiParam(value = "添加用户信息的对象",required = true) User user){
+    public Resp addUser(@RequestBody @ApiParam(value = "添加用户信息的对象",required = true) User user){
 
         System.out.println(user);
+
+        userserviceimpl.insertUsertotable(user);
+
+
+        Resp resp = new Resp(RespCode.SUCCESS,"哈哈");
+        return resp;
 
        // return user;
     }
 
+    @GetMapping(value = "/getTable")
+    @ApiOperation(value = "获取新闻列表")
+    public void getTable(){
 
+
+        userserviceimpl.parseList();
+
+//        return newsList.getList();
+    }
 
 }
--- src/main/java/com/unifs/behavioranalysis/service/impl/UserServiceImpl.java
+++ src/main/java/com/unifs/behavioranalysis/service/impl/UserServiceImpl.java
@@ -1,23 +1,44 @@
 package com.unifs.behavioranalysis.service.impl;
 
+import com.unifs.behavioranalysis.bean.AreaInfo;
 import com.unifs.behavioranalysis.bean.User;
+import com.unifs.behavioranalysis.dao.AreaInfoMapper;
 import com.unifs.behavioranalysis.dao.UserMapper;
 import com.unifs.behavioranalysis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class UserServiceImpl implements UserService
 {
 
     @Autowired
-    private UserMapper usermapper;
+    private UserMapper userMapper;
 
+    @Autowired
+    private AreaInfoMapper areaInfoMapper;
     @Override
     public List<User> getAllUser()
     {
+        return userMapper.selectAll();
+    }
+
+    @Override
+    public void insertUsertotable(User user)
+    {
+        userMapper.insert(user);
+    }
+
+    @Override
+    public void parseList()
+    {
+
+        List<AreaInfo> areaInfos = areaInfoMapper.selectByParentId("0");
+        System.out.println(areaInfos);
 
 
-        return null;
     }
 }
