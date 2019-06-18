54461b8d7ec067915da1887ed85e77341ad4034d
yaohuiru <1822018751@qq.com>
2019-06-18 16:31:03 +0800

    营业额&用户发展量走势图

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,17 +1,19 @@
 $(function(){
  	initPie();
  	initMap();
+ 	inittendencyInfo();
+ 	initcomparedInfo();
 })
 
 function initPie(){
 	var myChart = echarts.init(document.getElementById('pieGraph'));
 		pieoption = {
 	    title : {
-	        text: '某站点用户访问来源',
+	        text: '6月份业务受理情况',
 	        x:'center',
 	        textStyle: {
               fontSize: 16,
-              fontWeight: 'bolder',
+              fontWeight: 600,
               color: '#ff6600'
             },
 	    },
@@ -47,4 +49,99 @@ function initPie(){
         window.addEventListener("resize",function(){
 			myChart.resize();
        });
+}
+
+function initcomparedInfo() {
+    var myChart = echarts.init(document.getElementById('comparedInfo'));
+    comparedOption = {
+        title : {
+            text: '营业额同比交易量',
+            x:'center',
+            textStyle: {
+                fontSize: 16,
+                fontWeight: 600,
+                color: '#ff6600'
+            },
+        },
+        legend: {
+            bottom:'0',
+		},
+        tooltip: {
+
+		},
+        grid:{
+            x:40,
+            y:60,
+            x2:40,
+            y2:50,
+            borderWidth:1
+        },
+        dataset: {
+            dimensions: ['product', '2015', '2016', '2017'],
+            source: [
+                {product: 'Matcha Latte', '2015': 43.3, '2016': 85.8, '2017': 93.7},
+                {product: 'Milk Tea', '2015': 83.1, '2016': 73.4, '2017': 55.1},
+                {product: 'Cheese Cocoa', '2015': 86.4, '2016': 65.2, '2017': 82.5},
+                {product: 'Walnut Brownie', '2015': 72.4, '2016': 53.9, '2017': 39.1}
+            ]
+        },
+        xAxis: {type: 'category'},
+        yAxis: {},
+        // Declare several bar series, each will be mapped
+        // to a column of dataset.source by default.
+        series: [
+            {type: 'bar'},
+            {type: 'bar'},
+            {type: 'bar'}
+        ]
+    };
+    myChart.setOption(comparedOption,true);
+    window.addEventListener("resize",function(){
+        myChart.resize();
+    });
+}
+
+function inittendencyInfo() {
+    var myChart = echarts.init(document.getElementById('tendencyInfo'));
+    tendencyOption = {
+        title : {
+            text: '用户发展量走势图',
+            x:'center',
+            textStyle: {
+                fontSize: 16,
+                fontWeight: 600,
+                color: '#ff6600'
+            },
+        },
+        legend: {
+            bottom:'0',
+        },
+        tooltip: {
+
+        },
+        grid:{
+            x:70,
+            y:60,
+            x2:40,
+            y2:50,
+            borderWidth:1
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line',
+            areaStyle: {}
+        }]
+    };
+    myChart.setOption(tendencyOption,true);
+    window.addEventListener("resize",function(){
+        myChart.resize();
+    });
 }
