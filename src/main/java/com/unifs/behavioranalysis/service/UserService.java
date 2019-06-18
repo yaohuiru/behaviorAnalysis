bc422eb27e8a13fd4e277297098728244ca4163d
Fxz1991 <199111094@qq.com>
2019-06-18 11:02:36 +0800

    创建用户接口文件

--- src/main/java/com/unifs/behavioranalysis/service/UserService.java
+++ src/main/java/com/unifs/behavioranalysis/service/UserService.java
@@ -1,48 +1,29 @@
 package com.unifs.behavioranalysis.service;
 
-import com.unifs.behavioranalysis.bean.User;
-import com.unifs.behavioranalysis.dao.UserMapper;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
+import com.unifs.behavioranalysis.bean.User;
+
 import java.util.List;
 import java.util.UUID;
 
 /**
- * @Classname: UserService
+ * @Classname: UserService123
  * @User: 19911
- * @date: 2019/6/17 14:16
- * @discription: 用户管理Service层方法
+ * @date: 2019/6/18 10:55
+ * @discription: todo
  */
+public interface UserService {
 
-@Service
-public class UserService {
-    @Resource
-    private UserMapper userMapper;
+    //    新增用户：userinsert
+    public void userinsert(User user);
 
-
-//    新增用户：userinsert
-    public void userinsert(User user){
-        user.setUserId( UUID.randomUUID().toString().replace("-", ""));
-        userMapper.insert(user);
-    }
-//    删除用户：userdelete
-    public void  userdelete(String id){
-        userMapper.deleteByPrimaryKey(id);
-    }
-//    修改用户：userupdate
-    public void userupdate(User user){
-        userMapper.updateByPrimaryKey(user);
-    }
-//    查询所有：userselectall
-    public List<User> userselectall(){
-        return userMapper.selectAll();
-
-    }
-//    查询用户：userselect
-    public List<User> userselect(User user){
-        return userMapper.selectByPrimaryKey(user);
-    }
-
+    //    删除用户：userdelete
+    public void  userdelete(String id);
+    //    更新用户：userupdate
+    public void userupdate(User user);
+    //    查询所有：userselectall
+    public List<User> userselectall();
+    //    查询用户：userselect
+    public List<User> userselect(User user);
 
 }
