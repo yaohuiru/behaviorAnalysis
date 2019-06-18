66d8bc094f500341d787cee14da7fad00ed2c28b
yaohuiru <1822018751@qq.com>
2019-06-18 10:56:14 +0800

    用户发展统计

---
+++ src/main/java/com/unifs/behavioranalysis/controller/CountBusinessController.java
@@ -0,0 +1,22 @@
+package com.unifs.behavioranalysis.controller;
+
+import com.unifs.behavioranalysis.service.CountBusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CountBusinessController {
+    @Autowired
+    CountBusinessService countBusinessService;
+    @RequestMapping(value = "/countUserDevelop" ,method = RequestMethod.POST)
+    public int countUserDevelop(String orderDate){
+        int count=  countBusinessService.countUserDevelop("201906");
+        System.out.println(count);
+        return count;
+    }
+
+
+}
--- src/main/java/com/unifs/behavioranalysis/service/CountBusinessService.java
+++ src/main/java/com/unifs/behavioranalysis/service/CountBusinessService.java
@@ -1,4 +1,6 @@
 package com.unifs.behavioranalysis.service;
 
 public interface CountBusinessService {
+    //用户发展量
+    int countUserDevelop(String OrderDate);
 }
