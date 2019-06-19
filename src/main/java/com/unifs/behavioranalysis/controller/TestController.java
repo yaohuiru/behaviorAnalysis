b1582ee5e74df3604f33b6597b69ba32bd858976
Fxz1991 <199111094@qq.com>
2019-06-19 17:31:15 +0800

    lombok

--- src/main/java/com/unifs/behavioranalysis/controller/TestController.java
+++ src/main/java/com/unifs/behavioranalysis/controller/TestController.java
@@ -5,8 +5,6 @@ import com.unifs.behavioranalysis.base.Resp;
 import com.unifs.behavioranalysis.bean.User;
 import com.unifs.behavioranalysis.enums.RespCode;
 import com.unifs.behavioranalysis.service.TestService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,14 +18,12 @@ import org.springframework.web.bind.annotation.*;
  * @date 2019/4/25 18:05
  */
 @RestController
-@Api(value = "TestController", description = "测试控制层")    //swagger2生成API文档注解
 public class TestController {
     @Autowired
     private TestService testService;
 
     @ControlLog(operateType = "test", context = "AOP日志功能测试")
     @RequestMapping(value = "/test" ,method = RequestMethod.POST)
-    @ApiOperation(value = "根据用户编号获取用户姓名", notes = "test: 仅1和2有正确返回")
     public Resp test(@Validated @RequestBody User user) {
         Resp resp = new Resp(RespCode.SUCCESS);
         //testService.test01();
--- src/main/java/com/unifs/behavioranalysis/controller/UserController.java
+++ src/main/java/com/unifs/behavioranalysis/controller/UserController.java
@@ -5,8 +5,12 @@ import com.unifs.behavioranalysis.bean.User;
 import com.unifs.behavioranalysis.enums.RespCode;
 import com.unifs.behavioranalysis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.PrivateKey;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,12 +21,33 @@ import java.util.List;
  * @discription: todo
  */
 
-@RestController
+@Controller
 @RequestMapping("user")
 public class UserController
 {
     @Autowired
     private UserService userService;
+
+//    @RequestMapping(value = "/index")
+//    public String toIndex(){
+//        System.out.println("xxxxxx");
+//        return "/html/index";
+//    }
+//    登录
+    @PostMapping("login")
+    public String login(User user, HttpServletRequest request){
+       List<User> ans;
+       ans = userService.userselect(user);
+       request.getSession().setAttribute("User",ans.get(0));
+       switch (ans.get(0).getDepartmentId()) {
+           case "0":
+               return "html/admin";
+           default:
+               return "html/index";
+       }
+    }
+
+
 //    新增
     @PostMapping("insert")
     public String userinsert(@RequestBody User user){
@@ -31,7 +56,7 @@ public class UserController
     }
 //    条件查询
     @PostMapping("select")
-    public List<User> userselect( User user){
+    public List<User> userselect(@RequestBody User user){
         return userService.userselect(user);
     }
 //    查询所有数据
